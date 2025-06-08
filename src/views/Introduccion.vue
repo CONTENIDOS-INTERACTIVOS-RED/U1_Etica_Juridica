c0766243f2128a9b8b222b6938020b91fc219b66
Francisco Lizcano <flizcanor@icloud.com>
2025-06-08 11:25:10 -0500

    Intro ok

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -3,4 +3,65 @@
   BannerInterno(subTitulo="Introducción")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
 
+    .bg-full-width-2.bg-fondo-1
+      .px-4.px-md-5.py-4
+
+    
+
+        .row.mb-5
+          .col-lg-8
+            p(data-aos="fade-left").mb-4 En el ejercicio del Derecho, los profesionales no solo deben poseer un profundo conocimiento de las normas jurídicas, sino también un sólido compromiso con la ética y la moral. Estos conceptos aunque, con frecuencia, usados indistintamente, poseen diferencias fundamentales y raíces filosóficas que han sido objeto de reflexión desde la antigüedad. Comprender estas bases es esencial para formar abogados íntegros que ejerzan su profesión con responsabilidad, justicia y respeto, por los valores que sustentan la sociedad.
+
+        
+        
+            .bg-color-1.p-4(data-aos="fade-left").mb-4
+              .row.align-items-start
+                .col-lg-auto
+                  img(src="@/assets/curso/intro/1.svg", style="max-width: 90px").mx-auto
+                .col-lg
+                  p.mb-0 En esta unidad, se explorará la distinción entre ética y moral, sus fundamentos filosóficos y su relevancia en el ámbito jurídico. A través del Decálogo del Abogado, se enuncian los principios fundamentales que guían la conducta profesional del jurista, permitiendo su aplicación en situaciones concretas, para fortalecer el compromiso con la justicia y la ética profesional.
+        
+            p(data-aos="fade-left").mb-4 Al finalizar esta unidad, se espera que el estudiante correlacione las definiciones y concepciones filosóficas de la ética y la moral, comprendiendo su impacto en la práctica del Derecho. Además, aplicará los principios del Decálogo del Abogado en escenarios prácticos, demostrando un firme compromiso con los valores éticos y profesionales que rigen la abogacía.
+
+            p Para alcanzar este objetivo, la unidad se desarrollará en tres secciones principales:
+
+        
+          .col-lg-4
+            figure
+              img(src="@/assets/curso/intro/2.png", data-aos="zoom-in")
+
+        .row.justify-content-center.align-items-stretch
+          .col-lg-4.mb-4(data-aos="zoom-in-up")
+            .custom-image-card-2.h-100
+              img.custom-image-card__image(src="@/assets/curso/intro/3.png" alt="")
+              .custom-image-card__text.p-4
+                h5.mb-2.text-center Concepto de ética
+                p.mb-0.text-center Se examinarán las bases filosóficas de la ética, así como sus implicaciones en la conducta humana y profesional.
+          .col-lg-4.mb-4(data-aos="zoom-in-down")
+            .custom-image-card-2.h-100
+              img.custom-image-card__image(src="@/assets/curso/intro/4.png" alt="")
+              .custom-image-card__text.p-4
+                h5.mb-2.text-center Concepto de moral
+                p.mb-0.text-center Se explorará el concepto de moral, así como lo que representa en las sociedades y su aplicación.
+          .col-lg-4.mb-4(data-aos="zoom-in-down")
+            .custom-image-card-2.h-100
+              img.custom-image-card__image(src="@/assets/curso/intro/5.png" alt="")
+              .custom-image-card__text.p-4
+                h5.mb-2.text-center El Decálogo del Abogado
+                p.mb-0.text-center Se analizarán los principios fundamentales propuestos por Eduardo Couture, reflexionando sobre su importancia en la práctica del Derecho y su aplicación en casos concretos.
+
+
+    .row.mb-5.align-items-center
+      .col-lg-4
+        figure
+          img(src="@/assets/curso/intro/6.png", data-aos="zoom-in")
+      .col-lg-8
+        p(data-aos="fade-left").mb-4 El ejercicio del Derecho exige más que el dominio de las leyes y los procedimientos; requiere una sólida formación ética que garantice la confianza y el respeto en la profesión. A lo largo de esta unidad, se fomentará la reflexión crítica y el análisis de dilemas éticos, permitiendo que cada estudiante comprenda la relevancia de actuar con integridad en el ámbito jurídico. Se espera una participación activa, el intercambio de ideas y el análisis de situaciones reales, para fortalecer la capacidad de tomar decisiones alineadas con los principios de justicia y equidad.
+    
+        .bg-color-2.p-4(data-aos="fade-left")
+          p.mb-0 Con este enfoque, esta unidad no solo aportará conocimientos teóricos, sino que también permitirá desarrollar una visión ética del ejercicio profesional, promoviendo la responsabilidad y el compromiso con la justicia como valores esenciales en la práctica del Derecho.
+    
+        
+    
+
 </template>
